f9cc9d2f5b77f5d0c47f724ac463d901d0c2afc1
Pavel Rumkin <dev@rumk.in>
2017-08-29 02:10:45 +0300

    [docs] update module example

--- examples/module.lisp
+++ examples/module.lisp
@@ -1,26 +1,31 @@
 (
+  ; This is an example module
+  ; It exports default function print
+  ; define some constants, variables and sum function
   module
-  ; This module is about to print some message
-  ; This module is about to print some message
 
   (import io "io")
 
   (export default (
     func
-    print ; name
+    "print" ; name optional
     void ; return type
     [
       (string msg)
     ] ; args
     (io.print msg) ; body
   ))
-
-  (export (const numbers [1 2 3 5 7 11]))
-
+  ; constant string
   (export (const user0 "admin"))
-
-  (export (func sum int
-    [(int a) (int b)] ; args
-    (return (+ a b)) ; function body
-  ))
+  ; constant list
+  (export (const numbers [1 2 3 5 7 11]))
+  ; variable
+  (export (let count 0))
+  ; function as constant
+  (export (const sum (
+    func "sum" int [
+      (int a) (int b)
+    ]
+    (return (+ a b))
+  )))
 )
